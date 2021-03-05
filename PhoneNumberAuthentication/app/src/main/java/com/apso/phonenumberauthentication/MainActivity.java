72ceae7e3128ff6e92d7b2314e4eaf78f707927e
Piyush Ojha <32029236+PSOjha@users.noreply.github.com>
2021-03-04 21:17:19 -0800

    Add files via upload

---
+++ PhoneNumberAuthentication/app/src/main/java/com/apso/phonenumberauthentication/MainActivity.java
@@ -0,0 +1,57 @@
+package com.apso.phonenumberauthentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser mCurrentUser;
+
+    private Button mLogoutBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        mCurrentUser = mAuth.getCurrentUser();
+
+        mLogoutBtn = findViewById(R.id.logout_btn);
+
+        mLogoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mAuth.signOut();
+                sendUserToLogin();
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(mCurrentUser == null){
+            sendUserToLogin();
+        }
+    }
+
+    private void sendUserToLogin() {
+        Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(loginIntent);
+        finish();
+    }
+}
